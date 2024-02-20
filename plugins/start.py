2186a499d995673068868eedaed64838d4d0e308
Mrz-Botz <131391958+Mrz-Botz@users.noreply.github.com>
2024-02-20 17:30:26 +0530

    Done

---
+++ plugins/start.py
@@ -0,0 +1,32 @@
+from pyrogram import Client, filters, types as t
+from bot import StartTime
+
+startText = """
+ᴛʜᴇsᴇ ᴀʀᴇ sᴏᴍᴇ ᴀɪ ᴄᴏᴍᴍᴀɴᴅs
+
+ ➻ /draw : ᴄʀᴇᴀᴛᴇ ɪᴍᴀɢᴇs
+ ➻ /upscale : ᴜᴘsᴄᴀʟᴇ ʏᴏᴜʀ ɪᴍᴀɢᴇs
+ ➻ /gpt : ᴄʜᴀᴛɢᴘᴛ
+ ➻ /bard : ʙᴀʀᴅ ᴀɪ ʙʏ ɢᴏᴏɢʟᴇ
+ ➻ /mistral : ᴍɪsᴛʀᴀʟ ᴀɪ
+ ➻ /llama : ʟʟᴀᴍᴀ ʙʏ ᴍᴇᴛᴀ ᴀɪ
+ ➻ /palm : ᴘᴀʟᴍ ʙʏ ɢᴏᴏɢʟᴇ
+ ➻ /reverse : ʀᴇᴠᴇʀsᴇ ɪᴍᴀɢᴇ sᴇᴀʀᴄʜ
+ ➻ /gemini : ɢᴇᴍɪɴɪ ʙʏ ɢᴏᴏɢʟᴇ
+ ➻ /imagine : ᴄʀᴇᴀᴛᴇ ᴀɪ ɪᴍᴀɢᴇs
+
+ ➻ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ - @XBOTSUPPORTS 
+"""
+
+@Client.on_message(filters.command(["start","help","repo","source"]))
+async def start(_: Client, m: t.Message):
+    await m.reply_text(
+        startText,
+        reply_markup=t.InlineKeyboardMarkup(
+            [
+                [
+                    t.InlineKeyboardButton(text="Repo",url="https://github.com/Mrz-Botz/ADVANCE-AI-BOT ")
+                ]
+            ]
+        )
+    )
