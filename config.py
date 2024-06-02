3f8fd482f6383cc60eea0e217f7e6822cd7f40c3
82kpro_Official <161997419+UNNI12345YT@users.noreply.github.com>
2024-06-02 14:02:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config(object):
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
-    API_ID = os.getenv("API_ID")
-    API_HASH = os.getenv("API_HASH")
+    BOT_TOKEN = os.getenv("7406041849:AAHPK3rCTnKcbLRjTxHoTP_3joR1MHAR1MU")
+    API_ID = os.getenv("27408015")
+    API_HASH = os.getenv("2f07e7c921c8d2b982df12d65a46ca46")
     mediaPattern = r"\b(https?://(?:(.*?)\.)?(?:instagram\.com|instagr\.am|t\.co|twitter\.com)(?:[^\s]*))\b" #pin\.it|pinterest\.com|
