2fc91dae1077871cf23d731b37c107a1bc105d31
82kpro_Official <161997419+UNNI12345YT@users.noreply.github.com>
2024-06-02 14:15:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ load_dotenv()
 
 class Config(object):
     BOT_TOKEN = os.getenv("7406041849:AAHPK3rCTnKcbLRjTxHoTP_3joR1MHAR1MU")
-    API_ID = os.getenv("27408015")
-    API_HASH = os.getenv("2f07e7c921c8d2b982df12d65a46ca46")
+    API_ID = os.getenv("27500064")
+    API_HASH = os.getenv("690dc5633ef234f04f3825a7c1ad0272")
     mediaPattern = r"\b(https?://(?:(.*?)\.)?(?:instagram\.com|instagr\.am|t\.co|twitter\.com)(?:[^\s]*))\b" #pin\.it|pinterest\.com|
